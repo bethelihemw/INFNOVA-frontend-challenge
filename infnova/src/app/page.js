7d67debaf299b2b4cc52051d29b7971ef4716c77
Bethelihem Wondimneh <wondimnehbethelihem@gmail.com>
2026-02-19 17:40:31 +0300

    first commit

---
+++ infnova/src/app/page.js
@@ -0,0 +1,12 @@
+// import Image from "next/image";
+import Navbar from "@/components/Navbar";
+// import styles from "./page.module.css";
+export default function Home() {
+  return (
+    <main>
+      <Navbar />
+      
+      {/* <h1>INFNOVA Frontend Challenge</h1> */}
+    </main>
+  );
+}
---
+++ infnova/src/components/Navbar.jsx
@@ -0,0 +1,28 @@
+import styles from "./Navbar.module.css";
+
+export default function Navbar() {
+  return (
+    <nav className={styles.navbar}>
+      
+      
+      <div className={styles.logo}>
+        <img src="/image.png" alt="logo" />
+        {/* <span>INFNOVA</span> */}
+      </div>
+
+     
+      <ul className={styles.navLinks}>
+        <li>Courses</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+
+    
+      <div className={styles.actions}>
+        <a className={styles.signIn}>Sign In</a>
+        <button className={styles.enrollBtn}>Enroll Now</button>
+      </div>
+
+    </nav>
+  );
+}
